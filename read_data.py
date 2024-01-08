3d8f514767b8332aac62de55a9149d9c8a029d40
lorenzbranner <branner-lorenz@gmx.de>
2024-01-08 23:03:14 +0100

    read_data function, pca most stuff done

--- read_data.py
+++ read_data.py
@@ -4,36 +4,38 @@ import pandas as pd
 from scipy.signal import butter, lfilter
 import librosa
 
-db_file_name = "batcallsv14.db"
-table_name = "batcalls"
+def read_data(head):
+  db_file_name = "./batcallsv14.db"
+  table_name = "batcalls"
 
-data = pd.read_sql(table_name, f"sqlite:///{db_file_name}")
-data = data.groupby('bat').head(300)
-data['arr'] = data['arr'].map(lambda bytes: np.load(io.BytesIO(bytes)))
-data['arr'] = data['arr'].map(lambda call: call.astype(np.float32) / 2.0**(16-1))
+  data = pd.read_sql(table_name, f"sqlite:///{db_file_name}")
+  data = data.groupby('bat').head(300)
+  data['arr'] = data['arr'].map(lambda bytes: np.load(io.BytesIO(bytes)))
+  data['arr'] = data['arr'].map(lambda call: call.astype(np.float32) / 2.0**(16-1))
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
-  nyq = 0.5 * fs
-  low = lowcut / nyq
-  high = highcut / nyq
-  b, a = butter(order, [low, high], btype='band')
-  return b, a
+  def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-  b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-  y = lfilter(b, a, data)
-  return y
+  def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
-def fourier(data):
-  stft = np.abs(librosa.stft(data, n_fft=512, hop_length=32))
-  stft = 10 * np.log10(stft)
-  stft = np.nan_to_num(stft)
+  def fourier(data):
+    stft = np.abs(librosa.stft(data, n_fft=512, hop_length=32))
+    stft = 10 * np.log10(stft)
+    stft = np.nan_to_num(stft)
 
-  stft = (stft - np.min(stft)) / (np.max(stft) - np.min(stft))
-  stft = np.reshape(stft, (257, 138, 1))
-  stft = stft[:256, -128: , :]
-  return stft
+    stft = (stft - np.min(stft)) / (np.max(stft) - np.min(stft))
+    stft = np.reshape(stft, (257, 138, 1))
+    stft = stft[:256, -128: , :]
+    return stft
 
-data['arr'] = data['arr'].map(lambda call: butter_bandpass_filter(call, 1500, 12000, 44100, 5))
-data['arr'] = data['arr'].map(lambda call: (call - np.mean(call)) / np.std(call))
-data['arr'] = data['arr'].map(lambda call: fourier(call))
+  data['arr'] = data['arr'].map(lambda call: butter_bandpass_filter(call, 1500, 12000, 44100, 5))
+  data['arr'] = data['arr'].map(lambda call: (call - np.mean(call)) / np.std(call))
+  data['arr'] = data['arr'].map(lambda call: fourier(call))
+  return data
